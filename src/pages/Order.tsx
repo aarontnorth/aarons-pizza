dd1d531fd1ba1ed03b086f20c37b2486da8eb836
Aaron North <northtaaron@gmail.com>
2023-03-23 09:01:06 -0500

    Require fields when ordering pizza

--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -4,9 +4,16 @@ import TextFieldWithError from "../components/TextFieldWithError";
 import {PageWrapper} from "../components/PageWrapper";
 import {useContext} from "react";
 import OrderContext from "../contexts/OrderContext";
+import * as Yup from "yup";
 
 const Order = () => {
     const {createOrder} = useContext(OrderContext);
+
+    const orderSchema = Yup.object().shape({
+        crust: Yup.string().required('Crust is required'),
+        flavor: Yup.string().required('Flavor is required'),
+        size: Yup.string().required('Size is required'),
+    });
 
     // @ts-ignore
     const handleClick = (values) => {
@@ -21,20 +28,41 @@ const Order = () => {
     return (
         <PageWrapper heading={"Order a pie!"} subheading={"Customize your order"}>
             <Formik
+                validationSchema={orderSchema}
                 initialValues={{ crust: "Regular", flavor: "Cheese", size: "Medium" }}
                 onSubmit={(values, actions) => {
                     handleClick(values)
                     actions.setSubmitting(false);
                 }}
             >
-                <Form autoComplete={"off"}>
-                    <Field component={TextFieldWithError} name="crust" label={"crust"} />
-                    <Field component={TextFieldWithError} name="flavor" label={"flavor"} />
-                    <Field component={TextFieldWithError} name="size" label={"size"} />
-                    <StyledButton sx={{mt: 4, width: 'fit-content'}} type={'submit'}>
-                        Submit order
-                    </StyledButton>
-                </Form>
+                {({ errors, touched }) => (
+                    <Form autoComplete={"off"}>
+                        <Field
+                            component={TextFieldWithError}
+                            name="crust"
+                            label={"crust"}
+                            hasError={errors.crust && touched.crust}
+                            errorText={errors.crust}
+                        />
+                        <Field
+                            component={TextFieldWithError}
+                            name="flavor"
+                            label={"flavor"}
+                            hasError={errors.flavor && touched.flavor}
+                            errorText={errors.flavor}
+                        />
+                        <Field
+                            component={TextFieldWithError}
+                            name="size"
+                            label={"size"}
+                            hasError={errors.size && touched.size}
+                            errorText={errors.size}
+                        />
+                        <StyledButton sx={{mt: 4, width: 'fit-content'}} type={'submit'}>
+                            Submit order
+                        </StyledButton>
+                    </Form>
+                )}
             </Formik>
         </PageWrapper>
     )
